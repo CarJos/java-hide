6c240c7bd86e558b9262ff8bcd71c9c99bfe4d69
Carlos Jose Sanchez Peralta <csanchez@veritran.com>
2022-10-27 16:44:51 -0500

    Ejercicios random

---
+++ Libro/TestRan.java
@@ -0,0 +1,86 @@
+public class TestRan {
+    public static void main(String[] args) {
+        // int dado = 0, sum = 0;
+        // for (int i = 0; i < 3; i++) {
+        //     long valor;
+        //     valor = Math.round(Math.random() * 6) + 1; 
+        //     dado++;
+        //     sum += valor;
+        //     System.out.println("Dado "+ dado + ": " + valor);
+        // }
+        // System.out.println("Suma dados: " + sum);
+        // -----------------------------------------------
+        // -----------------------------------------------
+        // int valor = (int)Math.round(Math.random() * 13) + 1;
+        // int palos = (int)Math.round(Math.random() * 3);
+        // switch (valor) {
+        //     case 1:
+        //         System.out.print("As de ");
+        //         break;
+        //     case 2:
+        //         System.out.print("2 de ");
+        //         break;
+        //     case 3:
+        //         System.out.print("3 de ");
+        //         break;
+        //     case 4:
+        //         System.out.print("4 de ");
+        //         break;
+        //     case 5:
+        //         System.out.print("5 de ");
+        //         break;
+        //     case 6:
+        //         System.out.print("6 de ");
+        //         break;
+        //     case 7:
+        //         System.out.print("7 de ");
+        //         break;
+        //     case 8:
+        //         System.out.print("8 de ");
+        //         break;
+        //     case 9:
+        //         System.out.print("9 de ");
+        //         break;
+        //     case 10:
+        //         System.out.print("10 de ");
+        //         break;
+        //     case 11:
+        //         System.out.print("J de ");
+        //         break;
+        //     case 12:
+        //         System.out.print("Q de ");
+        //         break;
+        //     case 13:
+        //         System.out.print("K de ");
+        //         break;
+        //     default:
+        //         break;
+        // }
+        // switch (palos) {
+        //     case 0:
+        //         System.out.println("picas");
+        //         break;
+        //     case 1:
+        //         System.out.println("corazones");
+        //         break;
+        //     case 2:
+        //         System.out.println("diamantes");
+        //         break;
+        //     case 3:
+        //         System.out.println("treboles");
+        //         break;
+        //     default:
+        //         break;
+        // }
+        // -----------------------------------------------
+        // -----------------------------------------------
+        // for (int i = 0; i < 20; i++) {
+        //     System.out.print(Math.round(Math.random() * 10) + " ");
+        // }
+        // -----------------------------------------------
+        // -----------------------------------------------
+        for (int i = 0; i < 200; i++) {
+                System.out.print((Math.round(Math.random() * 99) + 100) + " ");
+            }
+    }
+}
